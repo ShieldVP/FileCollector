a0aa29eb945c5f950041eb7ff4e3b29842410902
Shield <yury2001@mail.ru>
2020-07-07 14:54:07 +0300

    Comments and documentation for every function added. Added a "акты выерки" type of documents.

--- converter.py
+++ converter.py
@@ -5,8 +5,12 @@ from spec_logging import logger
 
 @logger
 def convert(src):
+    """
+    Если файл файл не pdf, то конвертирует его в pdf через сохранение в Word.
+    """
     out_file = path.splitext(src)[0] + '.pdf'
     if src != out_file:
+        # TODO: Fix conversion errors.
         word = Dispatch('Word.Application')
         doc = word.Documents.Open(src)
         doc.SaveAs(out_file, FileFormat=17)  # wdFormatPDF = 17
--- file_collector.py
+++ file_collector.py
@@ -3,36 +3,78 @@
 from PyPDF2 import PdfFileReader as read, PdfFileWriter as create_pdf
 from os import mkdir, path
 from shutil import make_archive
+from functools import wraps
 import fitz
 
 
 # Functions
 
+def normalize_spaces(func):
+    """
+    Удаляет двойные пробелы, пробелы по краям в возвращаемой строке.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tmp = func(*args, **kwargs)
+        return ' '.join(tmp.split())
+    return wrapper
+
+
+@normalize_spaces
 def find_name(text, document_type):
+    """
+    Находит номер документа в зависимости от его типа.
+    Возвращает название файла (без двойных пробелов).
+    """
     if document_type == 1:
         for j in range(len(text)):
-            if text[j].startswith("Договор №") or \
-                    text[j].startswith("Государственный контракт №"):
-                return text[j].split()[1][1:] + " акт"
+            if text[j].startswith("Договор №"):
+                return text[j][9:-14] + " акт"
+            elif text[j].startswith("Государственный контракт №"):
+                return text[j][26:-14] + " акт"
     elif document_type == 2:
         for j in range(len(text)):
             if text[j] == "Договор " or text[j] == "контракт ":
                 return text[j + 1][1:-1] + " счёт"
     elif document_type == 3:
         for j in range(len(text)):
-            if text[j].startswith("Договор №") or \
-                    text[j].startswith("Государственный контракт №"):
-                return text[j].split()[2] + " с-ф"
+            if text[j].startswith("Договор №"):
+                return text[j][10:] + " с-ф"
+            elif text[j].startswith("Государственный контракт №"):
+                return text[j][27:] + " с-ф"
+    elif document_type == 4:
+        for j in range(len(text)):
+            if text[j].startswith("Договор №"):
+                return text[j][10:-15] + " ав"
+            elif text[j].startswith("Государственный контракт №"):
+                return text[j][27:-15] + " ав"
 
 
 def save_file(document, name, dir_name, errors, page):
+    """
+    Сохраняет файл с названием name, в папке dir_name.
+    Если такой файл уже есть, то сохраняет с добавлением индекса кратности.
+    В случае возникновения ошибки сохраняет их в errors.
+    """
     try:
-        document.write(open(path.join(dir_name, name + ".pdf"), "wb"))
+        if path.exists(path.join(dir_name, name + ".pdf")):
+            it = 1
+            new_name = name + f" ({it}).pdf"
+            while path.exists(path.join(dir_name, new_name)):
+                it += 1
+                new_name = name + f" ({it}).pdf"
+            document.write(open(path.join(dir_name, new_name), "wb"))
+        else:
+            document.write(open(path.join(dir_name, name + ".pdf"), "wb"))
     except:
         errors.append((1, page + 1))
 
 
 def extract_files(pdf, text_pdf, dir_name, document_type):
+    """
+    Функция проходит по каждой странице, запрашивает название у неё и
+    Сохраняет её как отдельный документ.
+    """
     errors = list()
     for i in range(pdf.getNumPages()):
         # Extracting current page
@@ -50,9 +92,16 @@ def extract_files(pdf, text_pdf, dir_name, document_type):
 
 def collect(working_directory, file_name, document_type):
     """
+    Выполнение основной задачи бота:
+    Разделение pdf файла на отдельные документы
+    с названиями - номерами документов.
+    Функция принимает параметры:
     Путь до рабочей директории,
     Название файла в формате pdf,
-    Тип документа: акт - 1, счёт - 2, счёт-фактуры - 3
+    Тип документа: акт - 1, счёт - 2, счёт-фактуры - 3, акт выверки - 4
+    Функция возвращает:
+    Адрес результирующего архива,
+    Список ошибок, возникших в ходе работы.
     """
 
     no_suffix_file_name = file_name[:-4]
